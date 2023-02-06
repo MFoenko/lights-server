8a6a933a1d221cb76a1ecbbe7ed3b427122408b5
mfoenko <mfoenko@gmail.com>
2023-02-06 18:09:34 -0500

    set up template project

---
+++ src/test/kotlin/com/mfoenko/leaderboard/LeaderboardApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mfoenko.leaderboard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LeaderboardApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
