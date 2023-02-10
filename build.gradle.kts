1abbd8016e40b83104207865c0816aee4866209c
mfoenko <mfoenko@gmail.com>
2023-02-10 08:54:52 -0500

    set up webapp submodule

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.github.gradle.node.npm.task.NpmTask
 
 plugins {
 	id("org.springframework.boot") version "3.0.2"
 	id("io.spring.dependency-management") version "1.1.0"
 	kotlin("jvm") version "1.7.22"
 	kotlin("plugin.spring") version "1.7.22"
+	id("com.github.node-gradle.node") version "3.5.1"
 }
 
 group = "com.example"
@@ -34,3 +36,30 @@ tasks.withType<KotlinCompile> {
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
+
+node {
+	download.set(true)
+	workDir.set(file("${project.buildDir}/nodejs"))
+	npmWorkDir.set(file("${project.buildDir}/npm"))
+}
+
+tasks.register<NpmTask>("appNpmInstall") {
+	description = "Installs all dependencies from package.json"
+	workingDir.set(file("${project.projectDir}/src/main/webapp"))
+	args.set(listOf("install"))
+}
+
+tasks.register<NpmTask>("appNpmBuild") {
+	description = "Builds production version of the webapp"
+	workingDir.set(file("${project.projectDir}/src/main/webapp"))
+	args.set(listOf("run", "build"))
+	dependsOn("appNpmInstall")
+}
+
+tasks.register<Copy>("copyWebApp") {
+	from("src/main/webapp/build")
+	into("build/resources/main/static/.")
+	dependsOn("appNpmBuild")
+}
+
+tasks.get("compileJava").dependsOn("copyWebApp")
